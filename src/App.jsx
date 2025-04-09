156ea53d7673d5c14fd6da51e721e07dab916deb
BhanuPrakashPandey0843 <bhanupandey0843@gmail.com>
2025-04-10 00:07:11 +0800

    made the file structure ready

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+      
+        <Navbar />
+        {/* Main content takes available space */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from "../components/Hero/Hero"
+import ShopCards from "../components/ShopCards/ShopCards"
+const LandingPage = () => {
+  return (
+    <div>
+     <Hero />
+     <ShopCards />
+    
+    
+    </div>
+  )
+}
+
+export default LandingPage
