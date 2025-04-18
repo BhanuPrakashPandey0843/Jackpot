c416252904747938a5e6f6bbebf554178db85b3c
BhanuPrakashPandey0843 <bhanupandey0843@gmail.com>
2025-04-18 12:35:13 +0530

    added the other page and skeleton to the project

---
+++ src/components/Moreproducts/Moreproducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Moreproducts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Moreproducts
---
+++ src/components/Upperheader/Upperheader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Upperheader = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Upperheader
